dfadae95e849f01b49a312b2b32a4ecb88dc2b1c
tamaritamari <tamari@minacare.co.jp>
2020-09-06 10:32:30 +0900

    だいたいわかった

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const now = new Date()
 const minuteAfter = new Date(now.getTime() + 60000)
 const whenSeptemberEnds = Math.floor(minuteAfter/1000.0) ;
 
+const globalMiddleWare = async({payload, context, next}) => {
+  console.log('globalMiddleWare')
+  await next()
+}
+
+app.use(globalMiddleWare)
 
 // wake me up うまくいかない
 // Error: An API error occurred: internal_error
@@ -61,6 +67,22 @@ app.message('hello', async({message, say}) => {
           },
           "action_id": "button_click"
         }
+      },
+      {
+        "block_id": "assign_ticket",
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Yahho!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "select_user"
+        }
       }
     ],
     text: `Hey there <@${message.user}>!`
@@ -73,6 +95,28 @@ app.action("button_click", async({body, ack, say}) => {
   await ack();
   await say(`<@${body.user.id}> clicked the button`);
 });
+app.action("test", async({body, ack, say}) => {
+  await ack();
+  await say(`uuuuuuuuuu`);
+});
+app.action({ action_id: 'select_user', block_id: 'assign_ticket' },
+  async ({ action, ack, context, respond }) => {
+    console.log("app.action({ action_id: 'select_user', block_id: 'assign_ticket' },")
+    await ack();
+    await respond("yahho", {replace_original: true})
+    // console.log(Object.entries(action))
+    // try {
+    //   const result = await app.client.reactions.add({
+    //     token: context.botToken,
+    //     name: 'white_check_mark',
+    //     timestamp: action.ts,
+    //     channel: action.channel.id
+    //   });
+    // }
+    // catch (error) {
+    //   console.error(error);
+    // }
+  });
 
 // おれbolt才能ない 
 // うまく動かない(https://slack.dev/bolt-js/ja-jp/concepts#event-listening)
@@ -91,6 +135,102 @@ app.action("button_click", async({body, ack, say}) => {
 //     console.error(error)
 //   }
 // });
+
+app.command('/open_modal', async({say, ack, command})=> {
+  ack()    
+  // 組み込みの WebClient を使って views.open API メソッドを呼び出す
+  await say(`${command.text}`);
+});
+
+app.command('/ticket', async({ack, body, client}) => {
+  await ack();
+  try {
+    const result = await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        type: 'modal',
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "Welcome to a modal with _blocks_"
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'input_c',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreames?'
+            },
+            element: {
+              type:'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
+          }
+        ],
+        submit: {
+          type:"plain_text",
+          text: 'Submit'
+        }
+      }
+    });
+    console.log(result)
+  }
+  catch(err){
+    console.error(err)
+  }
+});
+
+app.action('button_abc', async({ack, body, client}) => {
+  await ack();
+
+  try {
+    const result = await client.views.update({
+      view_id: body.view.id,
+      view: {
+        type: 'modal',
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text:'Updated modal'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'plain_text',
+              text: 'Updated modal'
+            }
+          },
+          {
+            type: 'image',
+            image_url: 'https://media.giphy.com/media/SVZGEcYt7brkFUyU90/giphy.gif',
+            alt_text: 'Yay! The modal was updated'
+          }
+        ]
+      }
+    });
+    console.log(result)
+  }catch(err){
+    console.error(err)
+  }
+});
 
 (async () => {
   await app.start(process.env.PORT || 3000)
